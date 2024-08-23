caadb86b8803703784c30c702d12026a38dcdfe8
tonyalonzoc <tonyalonzoc@gmail.com>
2024-08-23 15:48:17 -0600

    Fix

--- app/page.js
+++ app/page.js
@@ -62,7 +62,7 @@ export default function Home() {
           reimagined.
         </p>
       </div>
-
+ 
       <FileTypeFilter
         onApplyFilterClick={handleApplyFileTypeFilterClick}
       ></FileTypeFilter>
