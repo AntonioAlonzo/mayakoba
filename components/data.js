f147ecacca7b25bd2062c55d499f202ace8d2df8
tonyalonzoc <tonyalonzoc@gmail.com>
2026-05-13 15:42:31 -0600

    Upload summer of champions

--- components/data.js
+++ components/data.js
@@ -4045,13 +4045,22 @@ export const ASSETS = [
     type: "photos-meeting",
     filetype: "img",
   },
-   {
+  {
     id: 361,
     title: "Fairmont Summer of Champions",
     url: "https://www.fairmont-mayakoba.com/content/uploads/2026/05/Fairmont-Summer-of-Champions_v3_compressed.pdf",
     preview:
       "https://www.fairmont-mayakoba.com/content/uploads/2026/05/preview.png",
     type: "activities",
+    filetype: "other",
+  },
+  {
+    id: 361,
+    title: "Fairmont Summer of Champions",
+    url: "https://www.fairmont-mayakoba.com/content/uploads/2026/05/Fairmont-Summer-of-Champions_v3_compressed.pdf",
+    preview:
+      "https://www.fairmont-mayakoba.com/content/uploads/2026/05/preview.png",
+    type: "brochure",
     filetype: "other",
   },
 ];
