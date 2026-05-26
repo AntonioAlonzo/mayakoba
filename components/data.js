16c104aea9826444dc8e11a58a489647fbaf2b89
tonyalonzoc <tonyalonzoc@gmail.com>
2026-05-26 11:04:55 -0600

    Update spa price list

--- components/data.js
+++ components/data.js
@@ -1909,8 +1909,8 @@ export const ASSETS = [
   },
   {
     id: 262,
-    title: "Spa Price List ESP 22/05/26",
-    url: "https://www.fairmont-mayakoba.com/content/uploads/2026/05/Spa-Price-List-2026-ESP.pdf",
+    title: "Spa Price List ESP 26/05/26",
+    url: "https://www.fairmont-mayakoba.com/content/uploads/2026/05/Spa-Price-List-2026-ESP-26MAY2026.pdf",
     preview:
       "https://www.fairmont-mayakoba.com/content/uploads/2024/12/spa-pricelist-preview.png",
     type: "spa",
@@ -1919,7 +1919,7 @@ export const ASSETS = [
   {
     id: 263,
     title: "Spa Price List ENG 22/05/26",
-    url: "https://www.fairmont-mayakoba.com/content/uploads/2026/05/Spa-Price-List-2026-ING.pdf",
+    url: "https://www.fairmont-mayakoba.com/content/uploads/2026/05/Spa-Price-List-2026-ING-26MAY2026.pdf",
     preview:
       "https://www.fairmont-mayakoba.com/content/uploads/2024/12/spa-pricelist-preview.png",
     type: "spa",
